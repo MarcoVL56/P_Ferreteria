1ccb03b391093fc83d7f8858f84cdc6b227df825
KEVIN LOPEZ MEZA <kevin.lopez3@ulatina.net>
2020-07-29 20:01:12 -0600

    Miercoles 29 Julio Tabla Inventario 1.9 Versión

--- Proyecto_Ferretería/src/Inventario/JF_Inventario.java
+++ Proyecto_Ferretería/src/Inventario/JF_Inventario.java
@@ -121,18 +121,26 @@ public class JF_Inventario extends javax.swing.JFrame {
 
         jtInventario.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "ID", "Nombre", "Presentación", "Proveedor", "Precio", "I.V.A"
+                "ID", "Nombre", "Presentación", "Proveedor", "Precio"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jtInventario);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 260, 680, 419));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 260, 680, 120));
 
         PanelMenuI.setBackground(new java.awt.Color(204, 204, 204));
         PanelMenuI.setForeground(new java.awt.Color(255, 255, 255));
